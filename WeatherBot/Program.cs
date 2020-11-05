868ce7262d62c272d3e6f366bb9d7cb047e92323
Dzianis <kotickpro@gmail.com>
2020-11-05 12:13:51 +0300

    fixed bugs

--- WeatherBot/Program.cs
+++ WeatherBot/Program.cs
@@ -8,6 +8,7 @@ using Telegram.Bot.Args;
 using Telegram.Bot.Requests;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InlineQueryResults;
 
 namespace WeatherBot
 {
@@ -22,16 +23,16 @@ namespace WeatherBot
 
         public static void Main(string[] args)
         {
-            string webHook = "http://a1111-7831a.s2.deploy-f.com";
+            string webHook = "https://a1111-7831a.s2.deploy-f.com";
 
             //Инициализация бота
             var proxy = new HttpToSocks5Proxy("198.46.205.105", 1080);
-            client = new TelegramBotClient("1460640304:AAHeucVR2KqbcQKvPktCL_Wj2gTdXlVpCPs") { Timeout = TimeSpan.FromSeconds(10)};
+            client = new TelegramBotClient("1302247747:AAF4kJCZ7NAZmE0H649mtSsve6VpLUfix5Y") { Timeout = TimeSpan.FromSeconds(10)};
 
             var me = client.GetMeAsync().Result;
             Console.WriteLine($"Bot_Id: {me.Id} \nBot_Name: {me.FirstName} ");
 
-            client.SetWebhookAsync(webHook).Wait();
+            //client.SetWebhookAsync(webHook).Wait();
             client.OnMessage += Bot_OnMessage;
             client.OnMessageEdited += Bot_OnMessage;
             client.StartReceiving();
@@ -46,19 +47,23 @@ namespace WeatherBot
 
             if (message.Type == MessageType.Text)
             {
+                if (message.Text == "/start")
+                    return;
+
                 NameCity = message.Text;
                 Weather(NameCity);
 
-                await client.SendTextMessageAsync(message.Chat.Id, $"Temperature in {nameOfCity}: {Math.Round(tempOfCity)} °C");
+                Console.WriteLine(message.Text);
+                await client.SendTextMessageAsync(message.Chat.Id, $"Температура в {nameOfCity}: {Math.Round(tempOfCity)} °C");
             }
 
         }
 
         public static string Weather(string cityName)
         {
-            string url = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&unit=metric&appid=2351aaee5394613fc0d14424239de2bd";
+            string url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&unit=metric&appid=2351aaee5394613fc0d14424239de2bd";
             HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
-            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest?.GetResponse();
             string response;
 
             using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
